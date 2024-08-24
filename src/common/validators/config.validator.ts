afd6912049bdf3f0b8f975f6e5515d777e87312d
suk-6 <me@suk.kr>
2024-08-25 00:27:27 +0900

    chore: Add required JWT configuration to config validator

--- src/common/validators/config.validator.ts
+++ src/common/validators/config.validator.ts
@@ -4,4 +4,7 @@ export const ConfigValidator = Joi.object({
   NODE_ENV: Joi.string()
     .valid("development", "production", "test")
     .default("production"),
+  // JWT
+  JWT_SECRET: Joi.string().required(),
+  JWT_ACCESS_TOKEN_EXP: Joi.string().required(),
 });
